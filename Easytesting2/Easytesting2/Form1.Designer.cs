d80155a6b2579078d0bbdab332e8d849714f4d42
oslohes123 <ashleytyagi@hotmail.com>
2021-08-04 20:10:28 +0100

    Added files

---
+++ Easytesting2/Easytesting2/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace Easytesting2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkbx_AlwaysOntop = new System.Windows.Forms.CheckBox();
+            this.btn_Win = new System.Windows.Forms.Button();
+            this.btn_Loss = new System.Windows.Forms.Button();
+            this.txt_ATR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_AccountValue = new System.Windows.Forms.TextBox();
+            this.txt_Risk = new System.Windows.Forms.TextBox();
+            this.rtxt_Output = new System.Windows.Forms.RichTextBox();
+            this.btn_Calculate = new System.Windows.Forms.Button();
+            this.btn_Timer = new System.Windows.Forms.Button();
+            this.btn_Undo = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_Losses = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_Wins = new System.Windows.Forms.Label();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkbx_AlwaysOntop
+            // 
+            this.chkbx_AlwaysOntop.AutoSize = true;
+            this.chkbx_AlwaysOntop.Location = new System.Drawing.Point(12, 12);
+            this.chkbx_AlwaysOntop.Name = "chkbx_AlwaysOntop";
+            this.chkbx_AlwaysOntop.Size = new System.Drawing.Size(98, 17);
+            this.chkbx_AlwaysOntop.TabIndex = 1;
+            this.chkbx_AlwaysOntop.Text = "Always On Top";
+            this.chkbx_AlwaysOntop.UseVisualStyleBackColor = true;
+            this.chkbx_AlwaysOntop.CheckedChanged += new System.EventHandler(this.chkbx_AlwaysOntop_CheckedChanged);
+            // 
+            // btn_Win
+            // 
+            this.btn_Win.BackColor = System.Drawing.Color.Lime;
+            this.btn_Win.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_Win.FlatAppearance.BorderSize = 0;
+            this.btn_Win.Location = new System.Drawing.Point(-4, 35);
+            this.btn_Win.Name = "btn_Win";
+            this.btn_Win.Size = new System.Drawing.Size(88, 145);
+            this.btn_Win.TabIndex = 2;
+            this.btn_Win.Text = "Win";
+            this.btn_Win.UseVisualStyleBackColor = false;
+            this.btn_Win.Click += new System.EventHandler(this.btn_Win_Click);
+            // 
+            // btn_Loss
+            // 
+            this.btn_Loss.BackColor = System.Drawing.Color.Red;
+            this.btn_Loss.Location = new System.Drawing.Point(90, 35);
+            this.btn_Loss.Name = "btn_Loss";
+            this.btn_Loss.Size = new System.Drawing.Size(83, 145);
+            this.btn_Loss.TabIndex = 3;
+            this.btn_Loss.Text = "Loss";
+            this.btn_Loss.UseVisualStyleBackColor = false;
+            this.btn_Loss.Click += new System.EventHandler(this.btn_Loss_Click);
+            // 
+            // txt_ATR
+            // 
+            this.txt_ATR.Location = new System.Drawing.Point(251, 51);
+            this.txt_ATR.Name = "txt_ATR";
+            this.txt_ATR.Size = new System.Drawing.Size(116, 20);
+            this.txt_ATR.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ATR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "% Risk";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(270, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Account Value";
+            // 
+            // txt_AccountValue
+            // 
+            this.txt_AccountValue.Location = new System.Drawing.Point(251, 106);
+            this.txt_AccountValue.Name = "txt_AccountValue";
+            this.txt_AccountValue.Size = new System.Drawing.Size(116, 20);
+            this.txt_AccountValue.TabIndex = 10;
+            // 
+            // txt_Risk
+            // 
+            this.txt_Risk.Location = new System.Drawing.Point(251, 156);
+            this.txt_Risk.Name = "txt_Risk";
+            this.txt_Risk.Size = new System.Drawing.Size(116, 20);
+            this.txt_Risk.TabIndex = 11;
+            // 
+            // rtxt_Output
+            // 
+            this.rtxt_Output.Location = new System.Drawing.Point(245, 191);
+            this.rtxt_Output.Name = "rtxt_Output";
+            this.rtxt_Output.ReadOnly = true;
+            this.rtxt_Output.Size = new System.Drawing.Size(158, 75);
+            this.rtxt_Output.TabIndex = 12;
+            this.rtxt_Output.Text = "";
+            // 
+            // btn_Calculate
+            // 
+            this.btn_Calculate.Location = new System.Drawing.Point(147, 199);
+            this.btn_Calculate.Name = "btn_Calculate";
+            this.btn_Calculate.Size = new System.Drawing.Size(92, 67);
+            this.btn_Calculate.TabIndex = 13;
+            this.btn_Calculate.Text = "Calculate";
+            this.btn_Calculate.UseVisualStyleBackColor = true;
+            this.btn_Calculate.Click += new System.EventHandler(this.btn_Calculate_Click);
+            // 
+            // btn_Timer
+            // 
+            this.btn_Timer.Location = new System.Drawing.Point(116, 8);
+            this.btn_Timer.Name = "btn_Timer";
+            this.btn_Timer.Size = new System.Drawing.Size(130, 23);
+            this.btn_Timer.TabIndex = 14;
+            this.btn_Timer.Text = "Time till prevous click";
+            this.btn_Timer.UseVisualStyleBackColor = true;
+            this.btn_Timer.Click += new System.EventHandler(this.btn_Timer_Click);
+            // 
+            // btn_Undo
+            // 
+            this.btn_Undo.Location = new System.Drawing.Point(8, 186);
+            this.btn_Undo.Name = "btn_Undo";
+            this.btn_Undo.Size = new System.Drawing.Size(102, 23);
+            this.btn_Undo.TabIndex = 15;
+            this.btn_Undo.Text = "Undo Last Click";
+            this.btn_Undo.UseVisualStyleBackColor = true;
+            this.btn_Undo.Click += new System.EventHandler(this.btn_Undo_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(187, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Wins";
+            // 
+            // lbl_Losses
+            // 
+            this.lbl_Losses.AutoSize = true;
+            this.lbl_Losses.Location = new System.Drawing.Point(187, 114);
+            this.lbl_Losses.Name = "lbl_Losses";
+            this.lbl_Losses.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Losses.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(187, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Losses";
+            // 
+            // lbl_Wins
+            // 
+            this.lbl_Wins.AutoSize = true;
+            this.lbl_Wins.Location = new System.Drawing.Point(187, 68);
+            this.lbl_Wins.Name = "lbl_Wins";
+            this.lbl_Wins.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Wins.TabIndex = 19;
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(8, 235);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_Reset.TabIndex = 20;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 285);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.lbl_Wins);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl_Losses);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_Undo);
+            this.Controls.Add(this.btn_Timer);
+            this.Controls.Add(this.btn_Calculate);
+            this.Controls.Add(this.rtxt_Output);
+            this.Controls.Add(this.txt_Risk);
+            this.Controls.Add(this.txt_AccountValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ATR);
+            this.Controls.Add(this.btn_Loss);
+            this.Controls.Add(this.btn_Win);
+            this.Controls.Add(this.chkbx_AlwaysOntop);
+            this.Name = "Form1";
+            this.Text = "EasyTesting_2";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkbx_AlwaysOntop;
+        private System.Windows.Forms.Button btn_Win;
+        private System.Windows.Forms.Button btn_Loss;
+        private System.Windows.Forms.TextBox txt_ATR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_AccountValue;
+        private System.Windows.Forms.TextBox txt_Risk;
+        private System.Windows.Forms.Button btn_Calculate;
+        private System.Windows.Forms.Button btn_Timer;
+        private System.Windows.Forms.Button btn_Undo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_Losses;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_Wins;
+        private System.Windows.Forms.Button btn_Reset;
+        public System.Windows.Forms.RichTextBox rtxt_Output;
+    }
+}
+
---
+++ Easytesting2/Easytesting2/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Collections;
+
+
+namespace Easytesting2
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        enum TradeOutcome
+        {
+            Win,
+            Loss
+        }
+
+        int wins = 0;
+        int losses = 0;
+        Stack<TradeOutcome> Trades = new Stack<TradeOutcome>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chkbx_AlwaysOntop_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.TopMost == true)
+            {
+                this.TopMost = false;
+            }
+            else
+            {
+                this.TopMost = true;
+            }
+        }
+
+        private void btn_Calculate_Click(object sender, EventArgs e)
+        {
+            if (txt_AccountValue.Text.Length == 0 || txt_ATR.Text.Length == 0 || txt_Risk.Text.Length == 0)
+            {
+                MessageBox.Show("Make sure to enter data into all three of the boxes");
+                resettxtboxes();
+            }
+            else if (txt_AccountValue.Text.Any(char.IsLetter) || txt_ATR.Text.Any(char.IsLetter) || txt_Risk.Text.Any(char.IsLetter))
+            {
+                MessageBox.Show("Make sure to enter numbers into each of the fields and nothing else for the thing to work properly");
+                resettxtboxes();
+
+            }
+            else
+            {
+                double ATR = Convert.ToDouble(txt_ATR.Text);
+                double AccountValue = Convert.ToDouble(txt_AccountValue.Text);
+                double Risk = AccountValue * (Convert.ToDouble(txt_Risk.Text)/100) ;
+
+                double StopLoss = ATR * 1.5;
+                double TakeProfit = ATR;
+                double dollersperpip = Risk / StopLoss;
+                rtxt_Output.Text = $"Stop Loss: {StopLoss}\nTake Profit: {TakeProfit} \nDollers Per Pip: {dollersperpip.ToString("N3")} \nHalf of that is: {(dollersperpip/2).ToString("N3")}";
+
+
+            }
+        }
+
+        private void resettxtboxes() {
+            txt_AccountValue.Text = "";
+            txt_ATR.Text = "";
+            txt_Risk.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chkbx_AlwaysOntop.Checked = true;
+            this.TopMost = true;
+        }
+
+        private void btn_Win_Click(object sender, EventArgs e)
+        {
+            wins++;
+            stopwatch.Restart();
+            Trades.Push(TradeOutcome.Win);
+            UpdateLabels();
+        }
+     
+
+        private void btn_Loss_Click(object sender, EventArgs e)
+        {
+            losses++;
+            stopwatch.Restart();
+            Trades.Push(TradeOutcome.Loss);
+            UpdateLabels();
+        }
+        private void UpdateLabels()
+        {
+            lbl_Wins.Text = $"{wins}";
+            lbl_Losses.Text = $"{losses}";
+        }
+
+        private void btn_Timer_Click(object sender, EventArgs e)
+        {
+            string message;
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+          ts.Hours, ts.Minutes, ts.Seconds,
+          ts.Milliseconds / 10);
+
+            if (Trades.Peek() == TradeOutcome.Win)
+            {
+                message = $"{elapsedTime} and the last button was a win";
+            }
+            else
+            {
+                message = $"{elapsedTime} and the last button was a loss";
+
+            }
+            MessageBox.Show(message);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string output = $"Wins: {wins} Losses: {losses} at this time {DateTime.Now.ToString("G")}";
+            using (StreamWriter sw = new StreamWriter("Data.txt", append: true))
+            {
+                sw.WriteLine(output);
+            }
+            
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            wins = 0;
+            losses = 0;
+            UpdateLabels();
+            Trades.Clear();
+        }
+
+        private void btn_Undo_Click(object sender, EventArgs e)
+        {
+            if(Trades.Count == 0)
+            {
+                MessageBox.Show("No more trades to go back");
+            }
+            else if(Trades.Peek() == TradeOutcome.Win)
+            {
+                wins--;
+                stopwatch.Restart();
+                UpdateLabels();
+                Trades.Pop();
+            }
+            else if(Trades.Peek() == TradeOutcome.Loss)
+            {
+                losses--;
+                stopwatch.Restart();
+                UpdateLabels();
+                Trades.Pop();
+            }
+
+        }
+    }
+    
+}
